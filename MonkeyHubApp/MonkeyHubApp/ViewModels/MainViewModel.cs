0ee70e428585a44149a556b6c21d04e8220c549f
Mateus Demboski <mateusdemboski@gmail.com>
2017-05-16 20:10:38 -0300

    Todo o conteudo praticado até a aula do módulo 2 - 'Binding - Mecanismo de Binding'

---
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using MonkeyHubApp.Annotations;
+
+namespace MonkeyHubApp.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _descricao;
+
+        public string Descricao
+        {
+            get => _descricao;
+            set => SetProperty(ref _descricao, value);
+        }
+
+        public MainViewModel()
+        {
+            Descricao = "Olá mundo! Eu estou aqui!";
+
+            Task.Delay(3000).ContinueWith(t =>
+            {
+                Descricao = "Meu texto mudou";
+            });
+        }
+
+        
+    }
+}
